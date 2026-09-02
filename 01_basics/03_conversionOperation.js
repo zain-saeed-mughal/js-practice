5cdc2b97a1504f3b687d6bec88beb9fb238496c7
Zain Saeed Mughal <zainsaeedmughal4@gmail.com>
2026-09-02 22:15:01 +0500

    lecture 3 done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,8 @@
+let score = 44
+
+console.log(typeof score) // number
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
