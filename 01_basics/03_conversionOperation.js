0c2733b69bf86c5f61455d8570c7044844ba07c9
Zain Saeed Mughal <zainsaeedmughal4@gmail.com>
2026-09-03 00:24:10 +0500

    7 lectures done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,3 +15,33 @@ let someNumber = 34
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// now studying operations
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+7)
+console.log(2*7)
+console.log(2**7)
+console.log(2-7)
+console.log(2/7)
+console.log(2%7)
+
+str1 = "Hello"
+str2 = "World"
+console.log(str1 + str2)
+console.log(str1 + " " + str2)
+
+console.log(2 + "2") // 22
+console.log("2" + 2) // 22
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32
+console.log(2 + Number("2")) // 4
+console.log(2 + Number("2.5")) // 4.5
+console.log((2 + 4) - 6 * 10) // -54
+
+let gameCounter = 100
+gameCounter++ // 101
+console.log(gameCounter)
