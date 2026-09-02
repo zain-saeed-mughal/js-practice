08e0bd18554cbe3bf952160c9b53ec6c8fe37ade
Zain Saeed Mughal <zainsaeedmughal4@gmail.com>
2026-09-02 21:17:22 +0500

    2 lecturs done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+let accountName = "JohnDoe";
+const accountBalance = 1000;
+var accountType = "Savings";
+
+let accountName2 = "HowardSmith";
+const accountBalance2 = 13000;
+var accountType2 = "Checking";
+
+console.log("accountName: " + accountName);
+console.log("accountBalance: " + accountBalance);
+console.log("accountType: " + accountType);
+console.log("accountName2: " + accountName2);
+console.log("accountBalance2: " + accountBalance2);
+console.log("accountType2: " + accountType2);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+ "use strict";         
+// alert("Hello, World!");
+
+console.log("ZAIN SAEED MUGHAL")
+
+let name = "Zain Saeed Mughal"
+let age = 20
+let isLoggedIn = true
+
+// number contain 2 to power 53
+// bigint
+// string ""
+// boolean true/false
+// null is standalone value 
+// undefined
+// symbol is unique and immutable
+// object is collection of key value pairs
+
+console.log(typeof "Zain Saeed Mughal")
