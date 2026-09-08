cb793b8330d64f335bb5c107ed5d4273c5c1d129
Zain Saeed Mughal <zainsaeedmughal4@gmail.com>
2026-09-08 21:12:06 +0500

    lecture 12 done

---
+++ 01_basics/06_nums&maths.js
@@ -0,0 +1,37 @@
+// ++++++++++Numbers+++++++
+const score = 400
+
+const balance = new Number(1000)
+// console.log(score)
+// console.log(balance)
+
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const newNumber = 134.80
+// console.log(newNumber.toPrecision(3))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++=+++++Maths+++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.8))
+// console.log(Math.min(4, 2, 6, 8, 1))
+// console.log(Math.max(4, 2, 6, 8, 1))
+
+// console.log(Math.random())
+// console.log(Math.random()*10 + 1)
+// console.log((Math.random()*10) + 1)
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
+
+
